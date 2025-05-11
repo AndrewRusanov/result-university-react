17a727df956d2b0354466c8ccae5e1108bbd3d61
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-05-11 21:30:38 +0300

    feat: add useHover

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import UseHoverDemo from "./components/UseHoverDemo/UseHoverDemo";
 
 function App() {
-  return <div className="content" ></div>;
+  return (
+    <div className="content">
+      <UseHoverDemo />
+    </div>
+  );
 }
 
 export default App;
